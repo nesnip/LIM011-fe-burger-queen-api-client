723a56e79f54edeacc6bf9c5978cf765b55eb3a6
nesnip <hpmarines12@gmail.com>
2020-02-26 19:22:31 -0500

    modify extensions of js files

---
+++ src/containers/LoginApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import LoginForm from '../components/LoginForm';
+
+class LoginApp extends Component {
+  render() {
+    return (
+      <LoginForm />
+    );
+  }
+}
+console.log(Component);
+
+export default LoginApp;
