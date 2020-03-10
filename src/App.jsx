b91bac59d17dc3a5b2cde4c555509c04587fce43
nesnip <hpmarines12@gmail.com>
2020-03-10 14:58:21 -0500

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 import LoginApp from './containers/LoginApp';
+import Home from './containers/Home';
 
 const App = () => (
-  <>
-    <LoginApp />
-  </>
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <LoginApp />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
