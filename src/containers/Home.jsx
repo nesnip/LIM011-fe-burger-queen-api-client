b85f8a58773828f258994c9765d64d081ac5fa20
nesnip <hpmarines12@gmail.com>
2020-03-13 15:49:37 -0500

    add function handleClick to Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import Menu from '../components/Menu/Menu';
 import Item from '../components/Item/Item';
+import products from './products';
 
-const Home = () => (
-  <>
-    <Menu />
-    <Item />
-  </>
-);
+const Home = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    products().then((res) => {
+      // const nombres = res.products.map((el) => el.name);
+      res.products.forEach((el) => {
+        console.log(el.name);
+      });
+    });
+  };
+  const dataProduct = products().then((res) => {
+    res.products.forEach((el) => {
+      console.log(el.name);
+    });
+  });
+  return (
+    <>
+      <Menu
+        handleClick={handleClick}
+      />
+      <Item
+        dataProduct={dataProduct}
+      />
+    </>
+  );
+};
 
 export default Home;
