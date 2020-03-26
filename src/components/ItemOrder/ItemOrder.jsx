19e0e7d8982fcc6651ac1ed6b28849f3d4ee169e
nesnip <hpmarines12@gmail.com>
2020-03-26 18:35:26 -0500

    create table

--- src/components/ItemOrder/ItemOrder.jsx
+++ src/components/ItemOrder/ItemOrder.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './ItemOrder.css';
 
-const ItemOrder = ({ name, quantity, price }) => (
-  <ul>
-    <li>
-      <span>{name}</span>
-      <span>{quantity}</span>
-      <span>{price}</span>
-    </li>
-  </ul>
+const ItemOrder = ({ name, quantity, price, id }) => (
+  <tr>
+    <td>{name}</td>
+    <td>{quantity}</td>
+    <td>
+      S./
+      {price * quantity}
+    </td>
+  </tr>
 );
 
 export default ItemOrder;
