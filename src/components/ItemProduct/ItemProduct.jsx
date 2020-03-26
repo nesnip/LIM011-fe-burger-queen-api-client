abe3ea081754b4c22816b7106dc368e9bf6e9a32
nesnip <hpmarines12@gmail.com>
2020-03-26 15:29:31 -0500

    add add + less button

--- src/components/ItemProduct/ItemProduct.jsx
+++ src/components/ItemProduct/ItemProduct.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './ItemProduct.css';
 // import products from '../../containers/products';
-import plus from '../../assets/images/plus.svg';
-import less from '../../assets/images/less.svg';
 
 const ItemProduct = ({
   name, price, image, id, obj,
@@ -22,22 +20,23 @@ const ItemProduct = ({
           {price}
         </span>
       </div>
-      <div className="btn-container">
-        <img src={less} alt="less" className="btn-plus-less" />
-        <input type="text" placeholder="0" className="quantity" value={quantity} />
-        <img
-          src={plus}
-          alt="plus"
-          className="btn-plus-less"
+      <div className="btn-group btn-container" role="group" aria-label="Basic example">
+        <button type="button" className="btn btn-secondary">-</button>
+        {/* <button type="button" className="btn btn-secondary"></button> */}
+        <input type="text" placeholder="0" className="quantity btn btn-secondary" value={quantity} />
+        <button
+          type="button"
+          className="btn btn-secondary"
           onClick={() => {
             setQuantity(quantity + 1);
             console.log(quantity);
             console.log(obj);
             console.log(id);
           }}
-        />
+        >
+          +
+        </button>
       </div>
-      <span />
     </div>
   );
 };
