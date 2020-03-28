d0a0c3d9ba97df92205e0dff93150b6bc1c5d378
nesnip <hpmarines12@gmail.com>
2020-03-27 19:15:29 -0500

    add total price

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,46 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Orders.css';
 import ItemOrder from '../ItemOrder/ItemOrder';
 
 const Orders = ({
   dataOrder, deleteProduct,
-}) => (
-  <div className="container-order">
-    <div className="order-body">
-      <h4>Nueva Orden</h4>
-      <input type="text" className="txt-form" id="name-client" placeholder="Ingrese nombre del cliente" />
-      <input type="text" className="txt-form" id="numero-mesa" placeholder="N° de mesa" />
-      <table className="egt">
-        <thead>
-          <tr>
+}) => {
+  // const [total, setTotal] = useState(0);
+  let totalPrice = 0;
+  dataOrder.forEach((el) => {
+    totalPrice += el.price * el.cantidad;
+  });
+  return (
+    <div className="container-order">
+      <div className="order-body">
+        <h4>Nueva Orden</h4>
+        <input type="text" className="txt-form" id="name-client" placeholder="Ingrese nombre del cliente" />
+        <input type="text" className="txt-form" id="numero-mesa" placeholder="N° de mesa" />
+        <table className="egt">
+          <thead>
+            <tr>
 
-            <th>Producto</th>
+              <th>Producto</th>
 
-            <th>Cantidad</th>
+              <th>Cantidad</th>
 
-            <th>Precio</th>
+              <th>Precio</th>
 
-            <th>Eliminar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dataOrder.map((objOrder) => (
-            <ItemOrder
-              key={objOrder.id}
-              name={objOrder.name}
-              price={objOrder.price}
-              quantity={objOrder.cantidad}
-              id={objOrder.id}
-              deleteProduct={deleteProduct}
-            />
-          ))}
-        </tbody>
-      </table>
-      <h5> Total </h5>
-      <button className="btn-enviar" id="btn-enviar" type="submit">
-        Enviar a Cocina
-      </button>
+              <th>Eliminar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataOrder.map((objOrder) => (
+              <ItemOrder
+                key={objOrder.id}
+                name={objOrder.name}
+                price={objOrder.price}
+                quantity={objOrder.cantidad}
+                id={objOrder.id}
+                deleteProduct={deleteProduct}
+              />
+            ))}
+            <tr>
+              <th>Total</th>
+              <th />
+              <th>
+                S/.
+                {totalPrice}
+              </th>
+            </tr>
+          </tbody>
+        </table>
+        <button className="btn-enviar" id="btn-enviar" type="submit">
+          Enviar a Cocina
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Orders;
