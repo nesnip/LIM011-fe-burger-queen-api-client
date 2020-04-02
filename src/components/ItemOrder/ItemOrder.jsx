a799a32054a82af2d7886738d64c5ff979856657
nesnip <hpmarines12@gmail.com>
2020-04-02 01:01:27 -0500

    call deleteItem function

--- src/components/ItemOrder/ItemOrder.jsx
+++ src/components/ItemOrder/ItemOrder.jsx
@@ -3,23 +3,25 @@ import './ItemOrder.css';
 import PropTypes from 'prop-types';
 import trash from '../../assets/images/garbage.svg';
 
-const ItemOrder = ({ objOrder, deleteProduct }) => (
+const ItemOrder = ({
+  name, qty, price, id, deleteItem,
+}) => (
   <tr>
-    <td>{objOrder.name}</td>
-    <td>{objOrder.count}</td>
+    <td>{name}</td>
+    <td>{qty}</td>
     <td>
       S/.
-      {objOrder.price * objOrder.count}
+      {price * qty}
     </td>
     <td>
-      <span id="btn-deleted"><img id="trash" src={trash} alt="Eliminar" onClick={() => deleteProduct(objOrder.id)} /></span>
+      <span id="btn-deleted"><img id="trash" src={trash} alt="Eliminar" onClick={() => deleteItem(name)} /></span>
     </td>
   </tr>
 );
 
 ItemOrder.propTypes = {
   objOrder: PropTypes.shape({}).isRequired,
-  deleteProduct: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
 };
 
 export default ItemOrder;
