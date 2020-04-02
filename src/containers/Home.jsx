1be65f5544ee0821f02ed979c2c8c39c0b26f3de
nesnip <hpmarines12@gmail.com>
2020-04-02 01:09:56 -0500

    add viewAllOrder + sendOrder + handleName func

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,11 +3,16 @@ import Menu from '../components/Menu/Menu';
 import Header from '../components/Header/Header';
 import Orders from '../components/Orders/Orders';
 import getProducts from './products';
+import OrderKitchen from '../components/OrderKitchen/OrderKitchen';
+import AddOrders from '../components/Orders/AddOrders';
+import GetOrders from '../components/Orders/GetOrders';
 
 const Home = () => {
   const [dataProducts, setDataProducts] = useState([]);
   const [dataOrder, setDataOrder] = useState([]);
   const [category, setCategory] = useState('');
+  const [AlldataOrder, setAllDataOrder] = useState([]);
+  const [client, setClient] = useState('');
   const token = localStorage.getItem('token');
 
   const updateProducts = () => getProducts(token).then((res) => (category !== ''
@@ -22,35 +27,63 @@ const Home = () => {
     setCategory(type);
   };
 
-  const addOrder = (child) => {
-    setDataOrder(child);
+  const addOrder = (uniqueArray) => {
+    setDataOrder(uniqueArray);
   };
 
-  const deleteProduct = (idProduct) => {
-    const i = dataOrder.findIndex((obj) => obj.id === idProduct);
-    const tempDataOrder = dataOrder;
-    if (i >= 0) {
-      tempDataOrder.splice(i, 1);
-    }
-    setDataOrder(tempDataOrder);
-    console.log(dataOrder);
-    console.log(tempDataOrder);
+  const deleteProduct = (newOrders) => {
+    setDataOrder(newOrders);
   };
+
+  const viewAllOrder = () => {
+    GetOrders(localStorage.getItem('token')).then((NewDataOrders) => setAllDataOrder(NewDataOrders), console.log(AlldataOrder));
+  };
+
+  const sendOrder = () => {
+    const _id = '01';
+    if (!client) {
+      alert('Ingrese nombre del cliente');
+    }
+    AddOrders(token,
+      _id,
+      client,
+      dataOrder.map((objOrder) => ({ productId: objOrder._id, qty: objOrder.qty })))
+      .then((res) => console.log(res));
+  };
+
+  const handleName = (e) => {
+    setClient(e.target.value);
+  };
+  console.log(AlldataOrder);
 
   return (
-    <div>
-      <Header />
+    <>
+      <Header
+        viewAllOrder={viewAllOrder}
+      />
       <Menu
         handleClick={handleClick}
         dataProducts={dataProducts}
         addOrder={addOrder}
+        viewAllOrder={viewAllOrder}
       />
       <Orders
         dataOrder={dataOrder}
-        setDataOrder={setDataOrder}
         deleteProduct={deleteProduct}
+        sendOrder={sendOrder}
+        handleName={handleName}
       />
-    </div>
+      {AlldataOrder.map((objOrder) => (
+        <OrderKitchen
+          client={objOrder.client}
+          _id={objOrder._id}
+          qty={objOrder.products[0].qty}
+          dateEntry={objOrder.dateEntry}
+          status={objOrder.status}
+         // name={objOrder.products[0].product.name}
+        />
+      ))}
+    </>
   );
 };
 
