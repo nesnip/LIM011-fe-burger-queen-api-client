fae7446035114af789e68f32350ac3ad16c7ca7d
nesnip <hpmarines12@gmail.com>
2020-04-02 00:59:39 -0500

    add OrdersKitchen path

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import LoginApp from './containers/LoginApp';
 import Home from './containers/Home';
+import OrdersKitchen from './containers/OrdersKitchen';
 
 function App() {
   return (
     <BrowserRouter>
       <>
         <Route path="/Home" component={Home} />
+        <Route path="/OrdersKitchen" component={OrdersKitchen} />
         <Route exact path="/" component={LoginApp} />
       </>
     </BrowserRouter>
