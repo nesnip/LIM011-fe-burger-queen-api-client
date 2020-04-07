94bc44fb813574b1774df68e0843ff8664e4ab0c
nesnip <hpmarines12@gmail.com>
2020-04-07 13:30:55 -0500

    add ItemOrder test

---
+++ src/tests/ItemOrder.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  render, fireEvent, act,
+} from '@testing-library/react';
+import ItemOrder from '../components/ItemOrder/ItemOrder';
+
+describe('ItemOrder', () => {
+  it('Debería de mostrar cada producto', () => {
+    const objProduct = {
+      _id: '5e841b6a5e2f19002b8ab9c6',
+      name: 'Café con leche',
+      price: 7,
+      image: 'https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-cappuccino.jpg?$Product_Desktop$',
+      type: 'desayuno',
+      dateEntry: '2020-04-01T04:41:14.928Z',
+    };
+    const deleteProduct = jest.fn();
+    const container = render(<ItemOrder
+      name={objProduct.name}
+      qty={objProduct.qty}
+      price={objProduct.price}
+      _id={objProduct._id}
+      deleteProduct={deleteProduct}
+    />);
+    const deleteButton = container.getByAltText('Eliminar');
+
+    act(() => {
+      fireEvent.click(deleteButton);
+    });
+    expect(deleteProduct).toHaveBeenCalled();
+  });
+});
