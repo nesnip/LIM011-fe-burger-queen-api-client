53574d55361133c1969470d69f9ef65eafcc9f9d
nesnip <hpmarines12@gmail.com>
2020-04-08 11:53:37 -0500

    Updates

---
+++ precache-manifest.5ad93a3ebf496ed6862289ce9c7569e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb6b0ed586c4f363ab83cab40361323",
+    "url": "/LIM011-fe-burger-queen-api-client/index.html"
+  },
+  {
+    "revision": "fb6a422d78714d259c2b",
+    "url": "/LIM011-fe-burger-queen-api-client/static/css/main.7b041d03.chunk.css"
+  },
+  {
+    "revision": "9054bb28503e8ac06f4e",
+    "url": "/LIM011-fe-burger-queen-api-client/static/js/2.fadd4e52.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/LIM011-fe-burger-queen-api-client/static/js/2.fadd4e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6a422d78714d259c2b",
+    "url": "/LIM011-fe-burger-queen-api-client/static/js/main.9ff53eda.chunk.js"
+  },
+  {
+    "revision": "26d619461c1025c9641b",
+    "url": "/LIM011-fe-burger-queen-api-client/static/js/runtime-main.256ff597.js"
+  },
+  {
+    "revision": "c2c4839f08cf53db59f1bd04ce72f335",
+    "url": "/LIM011-fe-burger-queen-api-client/static/media/Thundergood.c2c4839f.ttf"
+  },
+  {
+    "revision": "be93cb1b8cd9bc0c6caad6b8f94cdcd2",
+    "url": "/LIM011-fe-burger-queen-api-client/static/media/burger.be93cb1b.png"
+  },
+  {
+    "revision": "ba809cf6938220047a45502c63e33b88",
+    "url": "/LIM011-fe-burger-queen-api-client/static/media/candado.ba809cf6.svg"
+  },
+  {
+    "revision": "6f9f5073e26635f4ae6b9322e7ca38ca",
+    "url": "/LIM011-fe-burger-queen-api-client/static/media/correo.6f9f5073.svg"
+  },
+  {
+    "revision": "56b66ec86cbf84a79ec7b87122ff26de",
+    "url": "/LIM011-fe-burger-queen-api-client/static/media/garbage.56b66ec8.svg"
+  }
+]);
